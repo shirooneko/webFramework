4d1e7669e4b0da29dbd3ce92b238b77e7969e3f7
Muhammad Apriyansyah <57085445+shirooneko@users.noreply.github.com>
2023-06-17 13:49:24 +0700

    xuaaa

--- PHP Framework - Membuat Tampilan Menarik Menggunakan Bootstrap/app/Views/film/table.php
+++ PHP Framework - Membuat Tampilan Menarik Menggunakan Bootstrap/app/Views/film/table.php
@@ -1,40 +1,6 @@
-<html lang="en">
+<?= $this->extend('layout/Layout') ?>
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>LK27</title>
-  <link rel="stylesheet" href="/assets/css/bootstrap.css"">
-    <link rel=" stylesheet" href="/assets/css/bootstrap.min.css">
-</head>
-
-<body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">LK27</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link " aria-current="page" href="/">Beranda</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/film">Semua Film</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Kategori Film</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Tentang Kami</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+<?= $this->section('content') ?>
 
   <div class="container mt-3">
     <div class="row">
@@ -64,9 +30,4 @@
     </div>
   </div>
 
-
-  <script src="/assets/js/bootstrap.js"></script>
-  <script src="/assets/js/bootstrap.min.js"></script>
-</body>
-
-</html>
+<?= $this->endSection() ?>
---
+++ PHP Framework - Membuat Tampilan Menarik Menggunakan Bootstrap/app/Views/layout/Layout.php
@@ -0,0 +1,45 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LK27</title>
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">LK27</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/">Beranda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/film">Semua Film</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/genre">Kategori Film</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/about">Tentang Kami</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
+        <?= $this->renderSection('content') ?>
+    </div>
+
+    <script src="/assets/js/bootstrap.min.js"></script>
+</body>
+
+</html>
