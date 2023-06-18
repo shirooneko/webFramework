237085d2388917a54cac48184f8297d789e2a5d6
Muhammad Apriyansyah <57085445+shirooneko@users.noreply.github.com>
2023-06-19 01:14:43 +0700

    xixixi

--- PHP Framework - Membuat Tampilan Menarik Menggunakan Bootstrap/vendor/composer/installed.php
+++ PHP Framework - Membuat Tampilan Menarik Menggunakan Bootstrap/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'codeigniter4/appstarter',
-        'pretty_version' => 'v4.3.5',
-        'version' => '4.3.5.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '96c892e7a1355d976df887c972458d4f2ec94e1a',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'codeigniter4/appstarter' => array(
-            'pretty_version' => 'v4.3.5',
-            'version' => '4.3.5.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '96c892e7a1355d976df887c972458d4f2ec94e1a',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
