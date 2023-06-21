350d4d9b41ff8d2fb3eb9f100f116ca957d724be
Muhammad Apriyansyah <57085445+shirooneko@users.noreply.github.com>
2023-06-21 07:26:42 +0700

    pertemuan13

---
+++ PHP Framework - CRUD/app/Controllers/Film.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+use App\Models\FilmModel;
+
+
+class Film extends BaseController
+{
+    protected $film;
+
+    public function __construct()
+    {
+        $this->film = new FilmModel();
+    }
+
+    public function index(){
+        $data['dataFilm'] = $this->film->getAllDataJoin();
+        return view("film/table", $data);
+    }
+
+    public function genre(){
+        $data['dataFilm'] = $this->film->getAllDataJoin();
+        return view("film/genre", $data);
+    }
+
+    public function all()
+    {
+        $data['data_film'] = $this->film->getAllDataJoin();
+        return view("film/index", $data);
+    }
+
+    public function findById()
+    {
+        dd($this->film->getDataById(1));
+    }
+
+    public function findByName()
+    {
+        dd($this->film->getDataBy('suzume'));
+    }
+
+    public function findByOrder()
+    {
+        dd($this->film->getOrderBy());
+    }
+
+    public function findLimit()
+    {
+        dd($this->film->getLimit());
+    }
+
+    public function findColumn()
+    {
+        dd($this->film->getFilmGenre());
+    }
+}
---
+++ PHP Framework - CRUD/app/Views/about/index.php
@@ -0,0 +1,29 @@
+<?= $this->extend('layout/Layout') ?>
+
+<?= $this->section('content') ?>
+
+  <div class="container">
+    <h1>Tentang Kami</h1>
+
+    <div style="text-align: justify;">
+    <p>
+        Selamat datang di website LK27, rumah bagi para pecinta film sejati! Kami dengan bangga mempersembahkan platform livestreaming film yang akan membawa Anda ke dalam dunia tak terbatas dari cerita-cerita menakjubkan, emosi yang menghentak, dan pengalaman sinematik yang luar biasa.
+    </p>
+        <p>Di LK27, kami mengerti betapa pentingnya hiburan dalam kehidupan sehari-hari. Oleh karena itu, kami berkomitmen untuk menyediakan layanan streaming film terbaik dengan kualitas yang tak tertandingi. Dengan antarmuka yang ramah pengguna dan teknologi canggih, kami memudahkan Anda menemukan film-film terbaru dan terbaik dari berbagai genre yang sesuai dengan selera Anda.</p>
+
+        <p>Apakah Anda mencari film aksi yang mendebarkan yang akan membuat jantung Anda berdegup kencang, drama yang memukau yang akan menggerakkan emosi Anda, komedi yang akan menghadirkan tawa segar di wajah Anda, atau petualangan yang akan membawa Anda ke dunia baru yang penuh keajaiban, di LK27, Anda akan menemukan semua itu dan lebih banyak lagi. Koleksi film kami diperbarui secara teratur untuk memastikan Anda tidak ketinggalan film-film terbaru dan terpopuler di layar lebar.</p>
+        <p>
+        Kami juga memberikan perhatian besar pada pengalaman pengguna yang nyaman dan memuaskan. Dengan fitur pencarian yang canggih, Anda dapat dengan mudah menemukan film-film favorit Anda berdasarkan judul, genre, sutradara, atau pemeran. Selain itu, ulasan dari pengguna lain akan membantu Anda mendapatkan perspektif yang lebih luas tentang film-film yang ingin Anda tonton.
+        </p>
+        <p>
+        Tidak hanya itu, LK27 juga menyediakan fitur penyesuaian preferensi pribadi, di mana Anda dapat mengatur profil dan mengatur rekomendasi film berdasarkan preferensi Anda sendiri. Dengan begitu, setiap kunjungan ke LK27 akan menjadi pengalaman yang disesuaikan dan memuaskan.
+        </p>
+        <p>
+        Kami bangga menjadi mitra Anda dalam menemukan dunia sinematik yang luar biasa. Dalam perjalanan menonton film, kami berharap dapat menghadirkan kegembiraan, inspirasi, dan momen-momen yang tak terlupakan. Kami berkomitmen untuk terus menyediakan layanan terbaik dan menjadi tujuan utama Anda dalam menikmati hiburan film secara online.
+        </p>
+
+    </div>
+  </div>
+
+
+<?= $this->endSection() ?>
